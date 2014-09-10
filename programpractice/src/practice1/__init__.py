58dfe634666b225c180d1f67fe84badcd73f42ec
Sean Safari <safaris1@tcnj.edu>
2014-09-09 22:27:01 -0400

    minor changes
    only use string in complement method

--- programpractice/src/practice1/__init__.py
+++ programpractice/src/practice1/__init__.py
@@ -1,21 +1,18 @@
 # solution to problem found at http://www.reddit.com/r/dailyprogrammer/comments/2cld8m/8042014_challenge_174_easy_thuemorse_sequences/
 
-
-
 def complement(n):
     start=True
-    num=0
-    for i in str(n):
-        currentval=int(i)
-        if currentval==0:
-            newval=1
+    num='0'
+    for i in n:
+        if i=='0':
+            newval='1'
         else:
-            newval=0
+            newval='0'
         if start:
-            num=str(newval)
+            num=newval
             start=False
         else:
-            num+=str(newval)
+            num+=newval
     return num
 
 startnum= 0
@@ -25,7 +22,7 @@ print("nth \t Sequence")
 print("===========================================================================")
 for j in range(0, loopnum, 1):
     print(str(iterationnum) +  "\t" + str(startnum))
-    num2=complement(startnum)
+    num2=complement(str(startnum))
     startnum=str(startnum)+ str(num2)
     iterationnum = iterationnum + 1
 
